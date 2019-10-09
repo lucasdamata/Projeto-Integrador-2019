f9e7adf3474e81e09663dcae1b90f382b161218d
Lucas M. Silva <lucasmsilva@unipam.edu.br>
2019-10-09 02:16:03 -0300

    Conexão ao banco de dados AWS

--- data/DataBase.php
+++ data/DataBase.php
@@ -1,7 +1,17 @@
 <?php
+class DataBase{
+    private $conexao;
+	 
+	public function getConexao(){
+	    $this->conexao =  new PDO('mysql:host=dbrvarquitetura.cjhghckflzqf.us-west-1.rds.amazonaws.com;port:3306;dbname=dbrvarquitetura','root','projeto2019');
+        $this->conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$this->conexao->setAttribute(PDO::ATTR_CASE, PDO::CASE_LOWER);
+		$this->conexao->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+		return $this->conexao;
+	}
 
-$dsn = 'mysql:host=localhost/phpmyadmin; dbname= db_rvarquitetura';
-$usuario = 'root';
-$senha = ' ss';
+    public function closeConnection(){
+        $this->$conexao = null;
+    }
 
-$conexao = new PDO($dsn, $usuario, $senha);
+}
